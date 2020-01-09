39898a55d37d1e5a70e2a2d6bd6dac102ed66ba1
lior Shine <shinelior@gmail.com>
2020-01-09 18:30:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
 # Your Code Here
+
+def map(array)
+  
+  new_array = []
+   i=0
+ while i < array.length
+    new_array.push (yield(array[i]))
+  end
+
+  new_array
+end
+
+[1,2 ,3,-9].map{|x|  x * -1}
+
+ def reduce(array,sv=nil)
+   if sv
+     sum = sv
+     i = 0 
+   else 
+     sum = array[0] 
+     i = 1
+   end 
+   while i < array.length 
+   sum = yield(sum,array[i])
+    i += 1  
+  end
+  sum 
+end 
+sv=100
+  [1,2,3].reduce{|x,sv|   sv+x}   
+    
+
+
+ 
+ 
+
